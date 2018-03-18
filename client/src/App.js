10c60c5bb3ff4be1a4c59644263403fffc807ef2
Brett Nelson <brettcnelson@gmail.com>
2018-03-17 21:58:18 -0400

    state for heroku test

--- client/src/App.js
+++ client/src/App.js
@@ -19,67 +19,69 @@ class App extends React.Component {
       accept: 'application/json'
     })
     .then(res => res.json())
-    .then(data => {
-      setTimeout(() => this.setState({api:data}), this.state.delay);
-    })
+    .then(data => setTimeout(() => this.setState({api:data}), this.state.delay))
     .catch(err => console.log('ERR:', err))
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.api !== this.state.api) {
-      // check api POST
-      fetch('/api/entries', {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        method: 'post',
-        body: JSON.stringify({"database":"default","dbCollection":"entries"})
-      })
-      .then(res => res.json())
-      .then(data => {
-        setTimeout(() => this.setState({post:data}), this.state.delay);
-      })
-     .catch(err => console.log('ERR:', err))
-    }
-    else if (prevState.post !== this.state.post) {
-      // check api GET
-      fetch('/api/entries', {
-        accept: 'application/json'
-      })
-      .then(res => res.json())
-      .then(res => {
-        setTimeout(() => this.setState({get:res}), this.state.delay);
-      })
-     .catch(err => console.log('ERR:', err))
-    }
-    else if (prevState.get !== this.state.get) {
-      // check api PUT
-      fetch('/api/entries/' + this.state.get[this.state.get.length-1]._id, {
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          method: 'put',
-          body: JSON.stringify({"database":"UPDATED default","dbCollection":"UPDATED entries"})
-        })
-        .then(res => res.json())
-        .then(data => {
-          setTimeout(() => this.setState({put:data}), this.state.delay);
-        })
-       .catch(err => console.log('ERR:', err))
-    }
-    else if (prevState.put !== this.state.put) {
-      // check api DELETE
-      fetch('/api/entries/' + this.state.get[this.state.get.length-1]._id, {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        method: 'delete'
-      })
-      .then(res => res.json())
-      .then(res => {
-        setTimeout(() => this.setState({delete:res}), this.state.delay);
-      })
-    }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.api !== this.state.api) {
+  //     // check api POST
+  //     fetch('/api/entries', {
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       method: 'post',
+  //       body: JSON.stringify({"database":"default","dbCollection":"entries"})
+  //     })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setTimeout(() => this.setState({post:data}), this.state.delay);
+  //     })
+  //    .catch(err => console.log('ERR:', err))
+  //   }
+  //   else if (prevState.post !== this.state.post) {
+  //     // check api GET
+  //     fetch('/api/entries', {
+  //       accept: 'application/json'
+  //     })
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       setTimeout(() => this.setState({get:res}), this.state.delay);
+  //     })
+  //    .catch(err => console.log('ERR:', err))
+  //   }
+  //   else if (prevState.get !== this.state.get) {
+  //     // check api PUT
+  //     fetch('/api/entries/' + this.state.get[this.state.get.length-1]._id, {
+  //         headers: {
+  //           'Content-Type': 'application/json'
+  //         },
+  //         method: 'put',
+  //         body: JSON.stringify({"database":"UPDATED","dbCollection":"UPDATED"})
+  //       })
+  //       .then(res => res.json())
+  //       .then(data => {
+  //         setTimeout(() => this.setState({put:data}), this.state.delay);
+  //       })
+  //      .catch(err => console.log('ERR:', err))
+  //   }
+  //   else if (prevState.put !== this.state.put) {
+  //     // check api DELETE
+  //     fetch('/api/entries/' + this.state.get[this.state.get.length-1]._id, {
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       method: 'delete'
+  //     })
+  //     .then(res => res.json())
+  //     .then(res => {
+  //       setTimeout(() => this.setState({delete:res}), this.state.delay);
+  //     })
+  //   }
+  // }
+
+  JSON(stateKey) {
+    return (<pre><code>{JSON.stringify(this.state[stateKey], null, '\t')}</code></pre>);
   }
 
   passFail(stateKey) {
@@ -91,24 +93,23 @@ class App extends React.Component {
   }
 
   render() {
-
     return (
       <div className="App">
         <div className="justify"><code className={this.passFail('api')}>/api</code></div>
         <div className="justify"><div>PROXY</div></div>
-        <div className="justify left"><code>{JSON.stringify(this.state.api)}</code></div>
+        <div className="justify left">{this.JSON('api')}</div>
         <div className="justify"><div className={this.passFail('post')}>C</div></div>
         <div className="justify"><div>POST</div></div>
-        <div className="justify left"><code>{JSON.stringify(this.state.post)}</code></div>
+        <div className="left">{this.JSON('post')}</div>
         <div className="justify"><div className={this.passFail('get')}>R</div></div>
         <div className="justify"><div>GET</div></div>
-        <div className="justify left"><code>{JSON.stringify(this.state.get)}</code></div>
+        <div className="left">{this.JSON('get')}</div>
         <div className="justify"><div className={this.passFail('put')}>U</div></div>
         <div className="justify"><div>PUT</div></div>
-        <div className="justify left"><code>{JSON.stringify(this.state.put)}</code></div>
+        <div className="left">{this.JSON('put')}</div>
         <div className="justify"><div className={this.passFail('delete')}>D</div></div>
         <div className="justify"><div>DELETE</div></div>
-        <div className="justify left"><code>{JSON.stringify(this.state.delete)}</code></div>
+        <div className="left">{this.JSON('delete')}</div>
         <div className="justify">{this.okay()}</div>
       </div>
     );
